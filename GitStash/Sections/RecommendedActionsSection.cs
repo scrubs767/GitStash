242363be6a0ede0594252a8ad4e5207d10a3cadd
scrubs767 <scrubs767@gmail.com>
2015-11-25 07:35:59 -0500

    Backed out "no changes" in title, dont like it.

--- GitStash/Sections/RecommendedActionsSection.cs
+++ GitStash/Sections/RecommendedActionsSection.cs
@@ -17,23 +17,13 @@ namespace GitStash.Sections
         public RecommendedActionsSection(IGitStashWrapper wrapper)
         {
             this.wrapper = wrapper;
-            wrapper.StashesChangedEvent += GitService_PropertyChanged;
-        }
-
-        private void GitService_PropertyChanged(object sender, StashesChangedEventArgs e)
-        {
-            if (wrapper.WorkingDirHasChanges())
-                Title = "Create Stash";
-            else
-                Title = "Create Stash (Nothing to Stash)";
-            Refresh();
         }
 
         public override void Initialize(object sender, SectionInitializeEventArgs e)
         {
             base.Initialize(sender, e);
             SectionContent = new RecommendedActionsControl(new RecommendedActionsViewModel(wrapper));
-            Title = "Create Stash (Nothing to Stash)";
+            Title = "Create Stash";
             IsVisible = true;
         }
         public override void Refresh()
