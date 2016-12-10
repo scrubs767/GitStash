59499d763c395c046092214571b545b7a2bd0935
Scrubs767 <scrubs767@gmail.com>
2016-12-10 15:42:58 -0500

    Colorized the nav item to mathc the green icon in the menu

--- GitStash/StashNavigationItem.cs
+++ GitStash/StashNavigationItem.cs
@@ -9,36 +9,35 @@ using Scrubs.TeamExplorer;
 using Microsoft.VisualStudio.TeamFoundation.Git.Extensibility;
 using SecondLanguage;
 using GitStash.Common;
+using System.Windows.Media;
 
 namespace GitStash
-{
+{    
     [TeamExplorerNavigationItem(GitStashPackage.StashNavigationItem, 1500, TargetPageId = GitStashPackage.StashPage)]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
     public class StashNavigationItem : TeamExplorerNavigationItemBase
     {
-
         private readonly ITeamExplorer teamExplorer;
         private readonly IGitExt gitService;
         Translator T;
+
         [ImportingConstructor]
         public StashNavigationItem([Import(typeof(SVsServiceProvider))] IServiceProvider serviceProvider)
         {
             ServiceProvider = serviceProvider;
-            this.Image = Resources.StashIcon;
-            this.IsVisible = false;
+
+            Image = Resources.StashIcon;
+            ArgbColor = UI.Colors.NavigationItemGreen.ToInt32();
+            IsVisible = true;       
+            IsEnabled = true;
             this.T = GetService<IGitStashTranslator>().Translator;
-            this.Text = T["Git Stash"];
+            Text = T["GitStash"];
             teamExplorer = GetService<ITeamExplorer>();
             gitService = (IGitExt)serviceProvider.GetService(typeof(IGitExt));
             teamExplorer.PropertyChanged += TeamExplorerOnPropertyChanged;
-            IsVisible = true;
         }
 
         private void TeamExplorerOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
-        {
-            UpdateVisible();
-        }
-
-        private void UpdateVisible()
         {
             IsEnabled = false;
             if (gitService != null && gitService.ActiveRepositories.Any())
---
+++ GitStash/UI/Colors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace GitStash.UI
+{
+    public class Colors : Scrubs.TeamExplorer.Colors
+    {
+        public static Color NavigationItemGreen = Color.FromArgb(0xFF, 0x4C, 0xB1, 0x22); // #FF22B14C
+        public static Color NavigationItemRed = Color.FromRgb(0xF0, 0x50, 0x33);
+    }
+}
